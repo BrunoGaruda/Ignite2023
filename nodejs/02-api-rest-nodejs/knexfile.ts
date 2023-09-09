c3b166f8b8a2b387a8d87ac62b773d75cec9f21e
BrunoGaruda <bruleorn@gmail.com>
2023-09-09 19:41:23 -0300

    utilizando o ts-node para reconhecer as configs para a criação das migrations

---
+++ nodejs/02-api-rest-nodejs/knexfile.ts
@@ -0,0 +1,13 @@
+require('ts-node/register')
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
