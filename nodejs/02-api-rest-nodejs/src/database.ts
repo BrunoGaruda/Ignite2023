5ae607ab30701c1e94f59a4ff0d75eb8cabebbca
BrunoGaruda <bruleorn@gmail.com>
2023-09-14 16:37:00 -0300

    trocando o caminho do filename para env

--- nodejs/02-api-rest-nodejs/src/database.ts
+++ nodejs/02-api-rest-nodejs/src/database.ts
@@ -1,9 +1,12 @@
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
+
+console.log(process.env)
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
